fa15edbc0ae95af49a2316c1520af971042ce18b
Lucas <57920717+L64@users.noreply.github.com>
2020-10-30 12:43:07 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Thanks to @Vandesm14 and Repl.it for the original template (@templates/Personal-Blog-Site) -->
+    <!-- Feed created using Feedlang -->
+    <!-- Feedlang was created by Lucas M.T. in 2020 -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" href="img/favicon.png" type="image/x-icon" />
+  </head>
+  <body>
+    <div class="content">
+      <main>
+        <?php include "feed.fwf"; ?>
+      </main>
+      <footer>
+        <article>
+          <p>2020-2020 <a href="https://lucas64.tk">Lucas</a>, based on <a href="https://repl.it/@templates/Personal-Blog-Site">Personal Blog Site</a></p>
+        </article>
+      </footer>
+    </body>
+  </div>
+</html>
