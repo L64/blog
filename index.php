d937f085188f976d2a5eb4a12167173bea81f021
Lucas <57920717+L64@users.noreply.github.com>
2020-11-03 09:49:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if(isset($_GET['p'])) {
       <title><?php echo $title; ?> by <?php echo $author; ?></title>
       <div class="art_content">
         <div style="display: flex;">
-          <h3 class="pubBack" style="margin: 10;"><a href="index.php"><i class="fa fa-arrow-circle-left fa-2x" aria-hidden="true"></i></a></h3>
+          <h3 class="pubBack" style="margin: 10;"><a href="<?php echo $_SERVER['PHP_SELF']; ?>"><i class="fa fa-arrow-circle-left fa-2x" aria-hidden="true"></i></a></h3>
           <div style="display: block;">
             <article>
               <h1 id="title"><?php echo $title; ?></h1>
@@ -66,15 +66,28 @@ if(isset($_GET['p'])) {
         </footer>
       </div>
       <a href="#top"><button class="bp downtoup_arrow"><i class="fa fa-arrow-circle-up fa-2x" aria-hidden="true"></i></button></a>
-      <?php if($theme == "light") { ?>
+      <?php
+      if (isset($_GET['p'])) {
+        if($theme == "light") { ?>
+      <a href="?p=<?php echo $_GET['p']; ?>&theme=dark"><button class="bp theme"><i class="fa fa-moon fa-2x" aria-hidden="true"></i></button></a>
+        <?php } elseif($theme == "dark") { ?>
+      <a href="?p=<?php echo $_GET['p']; ?>&theme=light"><button class="bp theme"><i class="fa fa-sun fa-2x" aria-hidden="true"></i></button></a>
+        <?php } else { ?>
+      <a href="?p=<?php echo $_GET['p']; ?>&theme=dark"><button class="bp theme"><i class="fa fa-moon fa-2x" aria-hidden="true"></i></button></a>
+        <?php
+        }
+       } else {
+         if($theme == "light") { ?>
       <a href="?theme=dark"><button class="bp theme"><i class="fa fa-moon fa-2x" aria-hidden="true"></i></button></a>
-      <?php } elseif($theme == "dark") { ?>
+        <?php } elseif($theme == "dark") { ?>
       <a href="?theme=light"><button class="bp theme"><i class="fa fa-sun fa-2x" aria-hidden="true"></i></button></a>
-      <?php } elseif($theme == "light") { ?>
+        <?php } else { ?>
       <a href="?theme=dark"><button class="bp theme"><i class="fa fa-moon fa-2x" aria-hidden="true"></i></button></a>
-      <?php } ?>
+       <?php
+        }
+       } ?>
     </div>
     <!-- Blog posts based on Change-the-Text -->
-    <!-- Coded by Lucas (lucas64.tk) -->
+    <!-- Coded by Lucas (lucas64.tk)-->
   </body>
 </html>
