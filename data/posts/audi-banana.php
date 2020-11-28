89810f43ee4c09e689c65823ef8ebd0d5ed4889d
Lucas <57920717+L64@users.noreply.github.com>
2020-11-28 16:59:27 -0300

    Add files via upload

---
+++ data/posts/audi-banana.php
@@ -0,0 +1,13 @@
+<?php
+  # Config
+  $author = "Lucas";
+  $author_webpage = "https://lucas64.tk";
+  $title = "Audi Banana";
+  $description = "\"\"\"offensive\"\"\" girl eating a banana infront a car";
+  $date = "24-11-2020";
+  $prestory = "what's the problem?";
+  $story = "
+    <img src=\"https://www.filo.news/img/2020/09/18/audicomercial_1.jpg\" alt=\"Image\"></img>
+    <p>Image courtesy of Audi</p>
+  ";
+?>
---
+++ data/posts/halloween-2020.php
@@ -0,0 +1,18 @@
+<?php
+  # Config
+  $author = "Lucas";
+  $author_webpage = "https://lucas64.tk";
+  $title = "It's the spooky day!!!";
+  $description = "<i>*spooky dance*</i>";
+  $date = "31-10-2020";
+  $prestory = "All the dates in this example are in DD/MM/YYYY";
+  $story = "
+  <iframe src=\"https://www.youtube.com/embed/PmzwhVE5Ly4\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>
+  <ul>
+    <li>Title: “<a href=\"https://www.youtube.com/watch?v=PmzwhVE5Ly4\">It's spooky month</a>”</li>
+    <li>Author: “<a href=\"https://www.youtube.com/c/srpelo\">Sr Pelo</a>”</li>
+    <li>Source: “<a href=\"https://www.youtube.com\">YouTube</a>”</li>
+    <li>License: “CC BY”</li>
+  </ul>
+    ";
+?>
